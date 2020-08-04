0d419010f3f25dfca8da6d7b26f1853d207c793a
Trevis McGill <trevismcgill25@gmail.com>
2020-08-04 04:59:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe
-attr_accessor :color, :size:, :material, :condtion
+attr_accessor :color, :size, :material, :condtion
 attr_reader :brand
 
 def initialize(brand)
